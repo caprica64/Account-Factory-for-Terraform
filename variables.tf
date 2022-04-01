6bc72d3f9753f9a114a93512155d63330bfd3d87
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-04-01 20:06:01 +0000

    Filled all AWS vars.

--- variables.tf
+++ variables.tf
@@ -3,17 +3,38 @@ variable "ct_management_account_id" {
   default = "291045144839"
 }
 
-variable "log_archive_account_id" {}
+variable "log_archive_account_id" {
+  type = string
+  default = "716856377128"
+}
 
-variable "audit_account_id" {}
+variable "audit_account_id" {
+  type = string
+  default = "354111917189"
+}
 
-variable "aft_management_account_id" {}
+variable "aft_management_account_id" {
+  type = string
+  default = "422822914274"
+}
 
-variable "github_username" {}
+variable "github_username" {
+  type = string
+  default = "caprica64"
+}
 
-variable "ct_home_region" {}
+variable "ct_home_region" {
+  type = string
+  default = "us-east-1"
+}
 
-variable "tf_backend_secondary_region" {}
+variable "tf_backend_secondary_region" {
+  type = string
+  default = "us-east-2"
+}
 
-variable "terraformtoken" {}
+variable "terraformtoken" {
+  type = string
+  default = 
+}
 
